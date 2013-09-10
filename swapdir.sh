6f5eccacada378bee813f98a0e833a9cf0596ad6
Kirill Berezin <enelar@develop-project.ru>
2013-09-10 08:08:10 +0400

    Moving all code to functions

--- swapdir.sh
+++ swapdir.sh
@@ -16,10 +16,14 @@ function TestDirectory
 TestDirectory $1
 TestDirectory $2
 
+function Swap
+{
+  dir=$(mktemp -d)
+  mv $1 $dir
+  mv $2 $1
+  base=$(basename $1)
+  mv $dir/$base $2
+  rmdir $dir
+}
 
-dir=$(mktemp -d)
-mv $1 $dir
-mv $2 $1
-base=$(basename $1)
-mv $dir/$base $2
-rmdir $dir
+Swap $1 $2
