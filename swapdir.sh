a880c9272d0cff1ab1073a89383071048ec44051
Kirill Berezin <enelar@develop-project.ru>
2013-09-10 08:06:59 +0400

    Simple parameter check
    Testing that directory`s is exsist

--- swapdir.sh
+++ swapdir.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+function TestDirectory 
+{
+  if [ -L "$1" ];
+  then
+    echo "Parameter '$1' is symlink"
+    exit
+  fi
+  if [ ! -d "$1" ];
+  then
+    echo "Parameter '$1' is not directory"
+    exit
+  fi
+}
+
+TestDirectory $1
+TestDirectory $2
+
+
 dir=$(mktemp -d)
 mv $1 $dir
 mv $2 $1
